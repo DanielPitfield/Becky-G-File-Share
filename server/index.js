34abef9fc06a29ce4424a64f36be16079df6ffbc
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-18 16:03:39 +0000

    fix image upload

--- server/index.js
+++ server/index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const path = require("path");
+
 const multer = require("multer");
-const MAX_FILE_SIZE_MB = 10; 
-// Where are the uploaded images stored?
-const upload = multer({ dest: "Images/", limits: { fileSize: Math.pow(MAX_FILE_SIZE_MB, 6) } });
+const MAX_FILE_SIZE_MB = 10;
 // The maximum number of files which can be uploaded at once
 const MAX_NUM_FILES = 10;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "Images/");
+  },
+  filename: function (req, file, cb) {
+    const formattedDate = new Intl.DateTimeFormat("en-GB", { dateStyle: "short", timeStyle: "medium" })
+      .format(new Date())
+      .replace(/[^0-9]/gi, "-");
+    cb(null, `${formattedDate}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage, limits: { fileSize: Math.pow(MAX_FILE_SIZE_MB, 6) } });
 
 app.post("/upload", upload.array("fileInput", MAX_NUM_FILES), (req, res) => {
   for (const file of req.files) {
