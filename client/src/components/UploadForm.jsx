608b51b0ec15feeca0ef2a4c861eda2a9aa9dd53
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-18 05:13:28 +0000

    destination directory

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -1,7 +1,7 @@
 const UploadForm = () => {
   return (
     <form action="/upload" method="POST" encType="multipart/form-data">
-      <input type="file" name="fileInput" />
+      <input type="file" name="fileInput" multiple />
       <input type="submit" value="Upload" />
     </form>
   );
--- server/index.js
+++ server/index.js
@@ -3,23 +3,17 @@ const app = express();
 const port = 5000;
 
 const multer = require("multer");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "Images");
-  },
-  filename: (req, file, cb) => {
+// Where are the uploaded images stored?
+const upload = multer({ dest: "Images/" });
+// The maximum number of files which can be uploaded at once
+const MAX_NUM_FILES = 10;
+
+app.post("/upload", upload.array("fileInput", MAX_NUM_FILES), (req, res) => {
+  for (const file of req.files) {
     console.log(file);
-    cb(null, `${new Date()} - ${file.originalname}`);
-  },
-});
-const upload = multer({ storage: storage });
+  }
 
-app.get("/upload", (req, res) => {
-  res.render("/upload");
-});
-
-app.post("/upload", upload.single("fileInput"), (req, res) => {
-  res.send("File uploaded");
+  res.send("Files uploaded");
 });
 
 app.listen(port, () => {
