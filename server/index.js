f3334882a73e5bdaed0c36a07a9dd25bbf8ddc73
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-18 02:05:19 +0000

    file upload

--- server/index.js
+++ server/index.js
@@ -2,8 +2,25 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const formidable = require("formidable");
+
 app.get("/", (req, res) => {
-  res.send("Hola! Me llamo Becky G!");
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/", (req, res) => {
+  const form = formidable.IncomingForm();
+  form.parse(req);
+
+  form.on("fileBegin", (name, file) => {
+    file.path = `${__dirname}/uploads/${file.name}`;
+  });
+
+  form.on("file", (name, file) => {
+    console.log(`Uploaded ${file.name}`);
+  });
+
+  res.send("Files uploaded");
 });
 
 app.listen(port, () => {
