8963fcf4a4c5ffe0ab0ee9cc7b6c3d306bebe46a
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-18 06:12:58 +0000

    show selected file names

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 const UploadForm = () => {
+  const [selectedFileNames, setSelectedFileNames] = useState("");
+
   return (
     <form className="file-upload-form" action="/upload" method="POST" encType="multipart/form-data">
       <label className="file-input">
-        <input type="file" name="fileInput" multiple />
+        <input type="file" name="fileInput" multiple onChange={(event) => setSelectedFileNames(event.target.value)} />
         Select Files
       </label>
+      <span className="selected-file-names">{selectedFileNames}</span>
 
       <input className="submit-button" type="submit" value="Upload" />
     </form>
